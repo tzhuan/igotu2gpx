0c62f24247ac78e594f28ecaae81cf15215dba9b
unknown <mh21@piware.de>
2009-07-01 13:03:00 +0200

    Snapshot ppa release changelog includes shortlog since last release.

--- tools/ubuntu-ppa-snapshots.sh
+++ tools/ubuntu-ppa-snapshots.sh
@@ -2,6 +2,7 @@
 
 [ -d src ] || exit 1
 
+LASTVERSION=0.2.2
 VERSION=0.2.90
 DATE=`date "+%Y%m%d%H%M"`
 
@@ -12,15 +13,32 @@ read
 
 bzr revert
 
+MSGFILE=$(tempfile) || exit
+trap "rm -f -- '$MSGFILE'" EXIT
+echo "  * Changes since version $LASTVERSION:" >> "$MSGFILE"
+COLUMNS=1000 bzr log --line -r tag:igotu2gpx-$LASTVERSION..-1 | head -n -1 | sed 's/^.*[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\} /    - /' >> "$MSGFILE"
+
+vi "$MSGFILE"
+
 TARBALL=-sa
 for dist in intrepid jaunty karmic; do
-    dch -v $VERSION+bzr$DATE-1ubuntu1~${dist}1 -D $dist "Released $dist bzr snapshot."
+    dch -v $VERSION+bzr$DATE-1ubuntu1~${dist}1 -D $dist "XXXTEMPXXX"
+    (
+	cat debian/changelog | sed -n '0,/XXXTEMPXXX/p'
+	echo "  * Released $dist bzr snapshot."
+	cat "$MSGFILE"
+	cat debian/changelog | sed -n '/XXXTEMPXXX/,$p'
+    ) | grep -v XXXTEMPXXX | sponge debian/changelog
+
     bzr commit -m "Temporary commit."
-    bzr builddeb --builder "debuild $TARBALL -S" || true
+    bzr builddeb --builder "debuild $TARBALL -S -pgnome-gpg -sgpg" || true
     bzr uncommit --force
     bzr revert
     TARBALL=-sd
 done
+
+rm -f -- "$MSGFILE"
+trap - EXIT
 
 echo "Really publish to launchpad?"
 read
