275f2fcab3103a47652124e257349e18c6548e72
Michael Hofmann <mh21@piware.de>
2009-08-05 00:49:42 +0200

    Working release script. Now just needs launchpad support for announcements and file uploads.

--- tools/all-launchpad-release.py
+++ tools/all-launchpad-release.py
@@ -1,90 +1,196 @@
 #!/usr/bin/python
+
+'''Igotu2gpx release script.
+
+Usage: %s <milestone-to-release>'''
+
+import os
+import sys
+import getopt
+import subprocess
+from datetime import datetime
+
 from launchpadlib.launchpad import Launchpad, STAGING_SERVICE_ROOT
 from launchpadlib.credentials import Credentials
-from datetime import datetime
-import subprocess
-import email
 
-seriesName = "0.3"
-milestoneName = "0.2.91"
+class MakeRelease:
+    def __init__(self):
+        self.launchpad = self.login()
+        self.project = self.launchpad.projects['igotu2gpx']
+        self.files = [{
+            'file_type': 'Code Release Tarball',
+            'content_type': 'application/x-gzip',
+            'name': 'igotu2gpx-%s.tar.gz',
+            'description': 'source distribution',
+            }, {
+            'file_type': 'Installer file',
+            'content_type': 'application/octet-stream',
+            'name': 'igotu2gpx-%s.dmg',
+            'description': 'Mac OS X installer',
+            }, {
+            'file_type': 'Installer file',
+            'content_type': 'application/octet-stream',
+            'name': 'igotu2gpx-%s.zip',
+            'description': 'Microsoft Windows release, zip file',
+            }]
+        self.announcement = '''%s
 
-credentials = Credentials()
-credentials.load(open("/home/mh21/.config/launchpadlib/credentials.txt"))
-cachedir = "/home/mh21/.cache/launchpadlib/"
-launchpad = Launchpad(credentials, STAGING_SERVICE_ROOT, cachedir)
+Igotu2gpx is a Linux/Mac OS X/Windows GUI and command line program to show the
+configuration, dump and clear the internal flash and decode the stored tracks
+and waypoints of a' MobileAction i-gotU USB GPS travel logger. It uses libusb,
+so no special kernel drivers are needed.
+%s
+Homepage: https://launchpad.net/igotu2gpx/
+Binary releases and source tarballs are available from https://launchpad.net/igotu2gpx/+download
+Ubuntu packages are available from %s
+Report bugs at https://bugs.launchpad.net/igotu2gpx/
 
-project = launchpad.projects['igotu2gpx']
-series = launchpad.load('https://api.staging.launchpad.net/beta/igotu2gpx/' + seriesName)
+Let me know how it goes. Comments, bugs, translations, bug fixes and feature
+requests welcome!'''
 
-milestone = None
-for m in series.active_milestones:
-    if m.name == milestoneName:
-        milestone = m
-        break
-if milestone == None:
-    print 'Creating milestone'
-    milestone = series.newMilestone(name=milestoneName, summary='First release candidate')
+    def splitVersion(self, milestone):
+        (major, minor, patch) = milestone.split('.')
+        details = patch.split('+')
+        patch = details[0]
+        details = details[1:]
+        return [int(major), int(minor), int(patch)] + details
 
-if milestone.release_link != None:
-    release = milestone.release
-else:
-    print 'Creating release'
-    release = milestone.createProductRelease(date_released=datetime.now(), release_notes=
-        "The first release candidate for igotu2gpx 0.3.0 has been released.\n\n"
-        "Igotu2gpx is a Linux/Mac OS X/Windows GUI and command line program to "
-        "show the configuration, dump and clear the internal flash and decode "
-        "the stored tracks and waypoints of a MobileAction i-gotU USB GPS "
-        "travel logger. It uses libusb, so no special kernel drivers are "
-        "needed.\n\n"
-        "Changes in %s:\n"
-        "  - Reworked UI.\n"
-        "  - Trackpoints split into multiple tracks.\n"
-        "  - Single tracks can be saved.\n"
-        "  - GT120 and GT200 purge support.\n"
-        "  - Localization (http://translations.launchpad.net/igotu2gpx).\n\n"
-        "Homepage: https://launchpad.net/igotu2gpx/\n"
-        "Binary releases and source tarballs are available from "
-        "https://launchpad.net/igotu2gpx/+download\n"
-        "Ubuntu packages are available from "
-        "https://launchpad.net/~igotu2gpx/+archive\n"
-        "Report bugs at https://bugs.launchpad.net/igotu2gpx/\n\n"
-        "Let me know how it goes. Comments, contributions, bugs, bug fixes and "
-        "feature requests welcome!" % milestoneName)
+    def seriesFromMilestone(self, milestone):
+        (major, minor, patch) = self.splitVersion(milestone)
+        if patch >= 90:
+            minor = minor + 1
+        return '%s.%s' % (major, minor)
 
-gpgCall = ['gnome-gpg', '--armor', '--sign', '--detach-sign']
+    def versionDescription(self, milestone):
+        (major, minor, patch) = self.splitVersion(milestone)
+        if patch >= 90:
+            finalVersion = '%s.%s.%s' % (major, minor + 1, 0)
+            if patch == 90:
+                versionDescription = 'a beta version'
+            elif patch >= 91:
+                rcNames = ['first', 'second', 'third', 'fourth', 'fifth',
+                        'sixth']
+                versionDescription = ('the %s release candidate' %
+                        rcNames[patch - 91])
+            return ('%s of igotu2gpx %s' % (versionDescription, finalVersion))
+        else:
+            return 'Igot2gpx %s' % milestone
 
-baseName = 'igotu2gpx-' + milestoneName
+    def repository(self, milestone):
+        (major, minor, patch) = milestone.split('.')
+        if patch >= 90:
+            return 'https://launchpad.net/~igotu2gpx/+archive'
+        else:
+            return 'https://launchpad.net/~igotu2gpx/+archive/rc-archive'
 
-files = [{
-    'type': 'Code Release Tarball',
-    'mime': 'application/x-gzip',
-    'name': baseName + '.tar.gz',
-    'description': 'source distribution',
-    }, {
-    'type': 'Installer file',
-    'mime': 'application/octet-stream',
-    'name': baseName + '.dmg',
-    'description': 'Mac OS X installer',
-    }, {
-    'type': 'Installer file',
-    'mime': 'application/octet-stream',
-    'name': baseName + '.zip',
-    'description': 'Microsoft Windows release, zip file',
-    }]
+    def firstLine(self, milestone):
+        return ('%s has been released.' %
+                self.versionDescription(milestone).capitalize())
 
-for file in files:
-    content = open(file['name'], 'r').read()
-    signatureName = file['name'] + '.asc'
-    signatureContent = subprocess.Popen(gpgCall, stdin=subprocess.PIPE,
+    def login(self):
+        cachedir = os.path.expanduser('~/.cache/launchpadlib')
+        if not os.path.exists(cachedir):
+            os.makedirs(cachedir, 0700)
+        credfile = os.path.expanduser('~/.config/launchpadlib/credentials.txt')
+        try:
+            credentials = Credentials()
+            credentials.load(open(credfile))
+            launchpad = Launchpad(credentials, STAGING_SERVICE_ROOT, cachedir)
+        except:
+            launchpad = Launchpad.get_token_and_login('igotu2gpx-release-script',
+                    STAGING_SERVICE_ROOT, cachedir)
+            launchpad.credentials.save(open(credfile, "w", 0600))
+        return launchpad
+
+    def addFile(self, release, filename, description, content_type, file_type):
+        print 'Adding ' + filename
+        with open(filename, 'r') as f: content = f.read()
+        signatureName = filename + '.asc'
+        signatureContent = subprocess.Popen(['gnome-gpg', '--armor', '--sign',
+            '--detach-sign'], stdin=subprocess.PIPE,
             stdout=subprocess.PIPE).communicate(content)[0]
-    print 'Uploading ' + file['name']
-    """ This does not work, as json cannot represent binary data
-    https://bugs.launchpad.net/launchpadlib/+bug/408605
-    uploadedFile = release.add_file(content_type=file['mime'],
-            description=file['description'],
-            filename=file['name'],
-            file_content=content,
-            file_type=file['type'],
-            signature_filename=signatureName,
-            signature_content=signatureContent)
-    """
+        with open(signatureName, 'w') as f: f.write(signatureContent)
+
+        ''' This does not work, as json cannot represent binary data
+        https://bugs.launchpad.net/launchpadlib/+bug/408605
+        uploadedFile = release.add_file(content_type=content_type,
+                description=description,
+                filename=filename,
+                file_content=content,
+                file_type=file_type,
+                signature_filename=signatureName,
+                signature_content=signatureContent)
+        '''
+
+    def addFiles(self, release):
+        releaseName = release.version
+        for file in self.files:
+            self.addFile(release, file['name'] % releaseName,
+                    file['description'], file['content_type'],
+                    file['file_type'])
+
+    def closeBugs(self, milestone):
+        (major, minor, patch) = self.splitVersion(milestone.name)
+        if patch >= 90:
+            print 'Not closing bugs for pre-release version'
+            return
+        for task in milestone.searchTasks(status='Fix Committed'):
+            print 'Marking %s as fix released.' % task.title
+            task.transitionToStatus(status='Fix Released')
+
+    def makeRelease(self, milestone_name, changelog):
+        series = self.launchpad.load('https://api.staging.launchpad.net/beta/igotu2gpx/' +
+                self.seriesFromMilestone(milestone_name))
+        milestone = None
+        for m in series.active_milestones:
+            if m.name == milestone_name:
+                milestone = m
+                break
+        if milestone is None:
+            print 'Creating milestone'
+            milestone = series.newMilestone(
+                    name=milestone_name,
+                    summary=self.versionDescription(milestone_name))
+
+        if milestone.release_link is not None:
+            release = milestone.release
+        else:
+            print 'Creating release'
+            release = milestone.createProductRelease(
+                    date_released=datetime.now(),
+                    release_notes=self.announcement % (self.firstLine(milestone_name),
+                        '', self.repository(milestone_name)),
+                    changelog=changelog)
+
+        self.addFiles(release)
+        self.closeBugs(milestone)
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], 'h', ['help'])
+        except getopt.error, msg:
+            raise Usage(msg)
+        for o, a in opts:
+            if o in ('-h', '--help'):
+                print __doc__ % argv[0]
+                return 0
+        if len(args) < 2:
+            raise Usage('need at least a milestone and a changelog as parameter')
+
+        makeRelease = MakeRelease()
+        makeRelease.makeRelease(args[0], args[1])
+
+    except Usage, err:
+        print >>sys.stderr, err.msg
+        print >>sys.stderr, 'for help use --help'
+        return 1
+
+if __name__ == '__main__':
+    sys.exit(main())
